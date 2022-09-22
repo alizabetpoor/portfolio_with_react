ae035ca9bea2849166a5456fce8f690b5195921c
ali.zabetpour <ali_0_zabetpoor@ymail.com>
2022-09-22 10:56:09 +0330

    feat/social complete social component add social and email component to the main page

--- src/components/Social/index.tsx
+++ src/components/Social/index.tsx
@@ -1,7 +1,25 @@
+import GithubSvg from '../Svg/GithubSvg';
+import { socialLinksAndSvg } from '../../static/social/socialLinksAndSvg';
 import styles from './style.module.scss';
+import LinkedInSvg from '../Svg/LinkedInSvg';
 
 const Social = () => {
-	return <div className={styles.social}></div>;
+	return (
+		<div className={styles.social}>
+			<div className={styles.social__links}>
+				{socialLinksAndSvg.map((social) => (
+					<a
+						target='_blank'
+						rel='noreferrer'
+						href={social.link}
+						key={social.id}>
+						<social.svg transform={true} />
+					</a>
+				))}
+			</div>
+			<div className={styles.social__line}></div>
+		</div>
+	);
 };
 
 export default Social;
